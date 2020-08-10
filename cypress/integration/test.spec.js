208f75f7d1f194c601caa18094b9a05b5dd1a884
Grace Tree <grace.tree@ecs-digital.co.uk>
2020-08-10 20:54:26 +0100

    works locally with visit /

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -2,7 +2,7 @@
 
 describe('CI test', () => {
     it('should run a test', () => {
-        cy.visit(Cypress.env('host'));
+        cy.visit(Cypress.env('CYPRESS_HOST'));
         cy.log('TESTING! Hello World');
         cy.get('[type="submit"]').should('be.visible');
     });
