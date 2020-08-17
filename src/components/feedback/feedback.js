1bc5bc4ecfabbc17a27361803570ddbaf445787c
Grace Tree <grace.tree@ecs-digital.co.uk>
2020-08-17 21:29:33 +0100

    adding redio buttons and finished css on feedback page

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -5,20 +5,61 @@ export default class Feedback extends Component {
     render() {
         return (
             <div>
-                <h1>Feedback Form</h1>
-                <form>
-                    <div class='row'>
-                        <div class='col-25'>
-                            <label for='comments'>Comments</label>
+                <div className='col'>
+                    <form>
+                        <div className='row'>
+                            <h1>Feedback Form</h1>
                         </div>
-                        <div class='col-75'>
-                            <textarea id='comments' placeholder='Enter your comments here.' style={{height:'200px'}} /> 
-                        </div> 
-                    </div>
-                    <div class='row'>
-                        <input type='submit' value ='Submit' />
-                    </div>
-                </form>
+                        <div className='row'>
+                            <div className="radio-check">
+                                <label className='label-75'>
+                                    <input
+                                        type="radio"
+                                        name="mood"
+                                        value="happy"
+                                        className="radio-check-input"
+                                    />
+                                    Happy
+                                </label>
+                            </div>
+                            <div className="radio-check">
+                                <label className='label-75'>
+                                    <input
+                                        type="radio"
+                                        name="mood"
+                                        value="sad"
+                                        className="radio-check-input"
+                                    />
+                                    Sad
+                                </label>
+                            </div>
+                            <div className="radio-check">
+                                <label className='label-75'>
+                                    <input
+                                        type="radio"
+                                        name="mood"
+                                        value="other"
+                                        className="radio-check-input"
+                                    />
+                                    Other
+                                </label>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-75'>
+                                <label className='label' htmlFor='comments'>Comments:</label>
+                            </div>
+                            <div className='col-75'>
+                                <textarea id='comments' placeholder='Enter your comments here.' style={{height:'200px'}} /> 
+                            </div> 
+                        </div>
+                        <div className='row-25'>
+                            <div className='col-25'>
+                                <input type='submit' value ='Submit' />
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
