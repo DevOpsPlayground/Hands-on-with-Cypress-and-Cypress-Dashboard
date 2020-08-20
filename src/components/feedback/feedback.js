4230d1be37806806276c639a24bde0108b18ae4f
Grace Tree <grace.tree@ecs-digital.co.uk>
2020-08-20 17:21:43 +0100

    stop page refresh

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -24,7 +24,8 @@ export default class Feedback extends Component {
         this.setState({comments: input.target.value});
     }
 
-    sendFeedback() {
+    sendFeedback(event) {
+        event.preventDefault()
         postFeedback(this.state);
     }
 
